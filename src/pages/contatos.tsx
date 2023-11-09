95ac2ee6c0f1f8cecadf8e8b75ae49c3c2bc43d2
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-11-09 14:40:15 -0300

    contato
    Testando repositorio commit

--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -2,6 +2,9 @@ export default function contatos(){
     return(
         <div>
             <h1>contatos</h1>
+            <h2>josueldesouzasanches@gmail.com</h2>
+            <h2>091991188082</h2>
+            <h2>Cameta-pa</h2>
             <a href="/">Home</a>
         </div>
     )
