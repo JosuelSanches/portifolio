1c622f1cadf8f02484dd873549c4643624692bf8
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-11-10 15:59:34 -0300

    Portifolio head e _document
    Layout de Projeto e manipulacao do head com next/head e next/_document

--- src/componets/Home/AboutMe.jsx
+++ src/componets/Home/AboutMe.jsx
@@ -12,7 +12,16 @@ export default function AboutMe(){
             </div>
             <ul>
                 <li style={{ backgroundColor: '#2F74C0', color: '#ffffff' }}>typescript</li>
+                <li style={{ backgroundColor: '#6BDDFA', color: '#000000' }}>react</li>
+                <li style={{ backgroundColor: '#EFD81D', color: '#000000' }}>javascript</li>
+                <li style={{ backgroundColor: '#000000', color: '#ffffff' }}>next-js</li>
             </ul>
+        </div>
+        <div>
+            <img src="https://avatars.githubusercontent.com/u/107212505?s=400&u=01e853216987527b9a033e242b05358e07bf3ca3&v=4" alt="Foto de Perfil de Josuel. Homem, cabelo preto"/>
+            <p>Oitavo semestre</p>  
+            <br />
+            Faculdade de Sistemas de Informacao         
         </div>
     </main>
     )
