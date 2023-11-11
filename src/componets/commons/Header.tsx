aa7e398b637f34f11178aef0cb23804c36a10a18
Josuel de Souza Sanches <107212505+JosuelSanches@users.noreply.github.com>
2023-11-11 16:53:21 -0300

    Fontes personalizadas
    Fontes personalizadas com next/font!

--- src/componets/commons/Header.tsx
+++ src/componets/commons/Header.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+    subsets: ["latin"],
+    weight: '500',
+
+});
 
 export default function Header(){
     return(
-             <header>
+             <header className={roboto.className}>
             <Link href={"/"}>
                 <Image src="https://svgsilh.com/svg/3273204.svg" width={55} height={55} alt="Icone da letra J, Logo de josuel"/>
             </Link>
--- src/componets/commons/Layout.tsx
+++ src/componets/commons/Layout.tsx
@@ -1,6 +1,12 @@
 
 import {ReactNode} from "react"
 import Header from "./Header";
+import { JetBrains_Mono } from "next/font/google";
+
+const jetBrains_mono = JetBrains_Mono({
+    subsets: ['latin'],
+    weight: '400'
+})
 
 interface LayoutProps{
     children: ReactNode;
@@ -8,9 +14,9 @@ interface LayoutProps{
 
 export function Layout({children}: LayoutProps){
     return(
-        <>
+        <div className={jetBrains_mono.className}>
         <Header/>
         {children}
-        </>
+        </div>
     )
 }
